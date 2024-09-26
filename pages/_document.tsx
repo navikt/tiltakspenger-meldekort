29aba01e25a1875fb3afee6641594ec076a1a61c
Henrietta Eide Bleness <h.bleness@gmail.com>
2024-09-26 14:41:57 +0200

    F - Legg på authentication og NAV Dekoratøren

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,46 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import {
+  DecoratorComponentsReact,
+  fetchDecoratorReact,
+} from "@navikt/nav-dekoratoren-moduler/ssr";
+import { Page } from "@navikt/ds-react";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document<{ Decorator: DecoratorComponentsReact }> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const Decorator = await fetchDecoratorReact({
+      env: "dev",
+    });
+
+    return { ...initialProps, Decorator };
+  }
+
+  render() {
+    const { Decorator } = this.props;
+
+    return (
+      <Html>
+        <Head>
+          <Decorator.HeadAssets />
+        </Head>
+        <body>
+          <Page footer={<Decorator.Footer />}>
+            <Decorator.Header />
+            <Main />
+            <Decorator.Scripts />
+            <NextScript />
+          </Page>
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
