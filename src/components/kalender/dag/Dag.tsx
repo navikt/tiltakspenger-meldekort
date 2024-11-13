06b0792d764c914de48b92769852b0edb57008e7
Anders Nomerstad <anders@nomerstad.com>
2024-11-13 18:24:12 +0100

    Fargekoder for dag-status

--- src/components/kalender/dag/Dag.tsx
+++ src/components/kalender/dag/Dag.tsx
@@ -1,9 +1,23 @@
 import { format } from 'date-fns';
 import { MeldekortDag } from '@typer/meldekort-utfylling';
 import classNames from 'classnames';
+import { useMeldekortUtfylling } from '@context/meldekort-utfylling/useMeldekortUtfylling';
 
 import style from './Dag.module.css';
-import { useMeldekortUtfylling } from '@context/meldekort-utfylling/useMeldekortUtfylling';
+
+const getStatusStyle = (dag: MeldekortDag) => {
+    const { deltattValg, underValg } = dag.status;
+
+    if (deltattValg === 'deltatt') {
+        return style.deltatt;
+    }
+
+    if (deltattValg === 'ikkeDeltatt') {
+        return underValg === 'IKKE_DELTATT' ? style.ikkeDeltatt : style.fravær;
+    }
+
+    return style.ikkeValgt;
+};
 
 type Props = {
     dag: MeldekortDag;
@@ -11,16 +25,16 @@ type Props = {
 };
 
 export const Dag = ({ dag, readonly }: Props) => {
-    const { setValgtMeldekortDag } = useMeldekortUtfylling()
+    const { setValgtMeldekortDag } = useMeldekortUtfylling();
 
     const formattedDate = `${format(dag.dato, 'd')}.`;
 
     return (
         <td key={dag.dato} className={classNames(style.datoKontainer)}>
             <button
-                className={classNames(style.dato)}
+                className={classNames(style.dato, getStatusStyle(dag))}
                 onClick={() => {
-                    setValgtMeldekortDag(dag)
+                    setValgtMeldekortDag(dag);
                 }}
                 disabled={readonly}
             >
