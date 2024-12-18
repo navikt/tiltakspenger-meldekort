fcc58ab4cd3299f1a47e80adef4ba0554b84147e
SolviAndersen <37183652+SolviAndersen@users.noreply.github.com>
2024-12-18 10:57:44 +0100

    Update FraværHjelp.tsx

--- src/components/fyll-ut/hjelp/FraværHjelp.tsx
+++ src/components/fyll-ut/hjelp/FraværHjelp.tsx
@@ -4,18 +4,48 @@ import style from './Hjelp.module.css';
 
 export const FraværHjelp = () => {
     return (
-        <ReadMore header={'Hjelp til utfylling'} className={style.hjelp}>
+        <ReadMore header={'Les mer om hvordan du skal føre fravær'} className={style.hjelp}>
             <Heading size={'medium'} level={'3'}>
-                {'Dette er en header'}
-            </Heading>
-            <BodyLong spacing>{'Dette er en paragraf med avstand til neste paragraf'}</BodyLong>
-            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
-            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
+                {'Syk'}
+            </Heading>            
             <ul>
                 <li>{'Dette er et listeelement'}</li>
                 <li>{'Dette er et listeelement'}</li>
                 <li>{'Dette er et listeelement'}</li>
             </ul>
+            
+            <Heading size={'medium'} level={'3'}>
+                {'Sykt barn eller syk barnepasser'}
+            </Heading>            
+            <ul>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+            </ul>
+            <Heading size={'medium'} level={'3'}>
+                {'Annet fravær'}
+            </Heading>            
+            <ul>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+            </ul>
+            <Heading size={'medium'} level={'3'}>
+                {'Du kan ha rett til tiltakspenger ved annet fravær'}
+            </Heading>            
+            <ul>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+                <li>{'Dette er et listeelement'}</li>
+            </ul>
+
+
+
+
+
+            <BodyLong spacing>{'Dette er en paragraf med avstand til neste paragraf'}</BodyLong>
+            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
+            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
         </ReadMore>
     );
 };
