ed97a4f43af156685068d27f8a5067ae8d787e06
SolviAndersen <37183652+SolviAndersen@users.noreply.github.com>
2024-12-18 15:11:48 +0100

    Update DeltattHjelp.tsx

--- src/components/fyll-ut/hjelp/DeltattHjelp.tsx
+++ src/components/fyll-ut/hjelp/DeltattHjelp.tsx
@@ -5,12 +5,8 @@ import style from './Hjelp.module.css';
 export const DeltattHjelp = () => {
     return (
         <ReadMore header={'Les mer om hva som skal føres på meldekortet'} className={style.hjelp}>
-            <Heading size={'medium'} level={'3'}>
-                {'Dette er en header'}
-            </Heading>
-            <BodyLong spacing>{'For hver tiltaksdag i meldeperioden må du angi om du har deltatt eller ikke. Du kan ha rett til tiltakspenger ved enkelte fravær. Det er derfor viktig at du melder for alle avtalte tiltaksdager. Du skal ikke oppgi noe for dager som er utenfor tiltaket.'}</BodyLong>
-            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
-            <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
+            
+            <BodyLong spacing>{'For hver tiltaksdag i meldeperioden må du angi om du har deltatt eller ikke. Du kan ha rett til tiltakspenger ved enkelte fravær. Det er derfor viktig at du melder for alle avtalte tiltaksdager. Du skal ikke oppgi noe for dager som er utenfor tiltaket.'}</BodyLong>            
 
             <Heading size={'small'} level={'3'}>
                 {'Når skal du føre at du har deltatt?'}
@@ -25,14 +21,14 @@ export const DeltattHjelp = () => {
              <Heading size={'small'} level={'3'}>
                 {'Når skal du melde om fravær?'}
             </Heading>
-            <BodyLong spacing>{'Syk'}</BodyLong>
+            <BodyLong>{'Syk'}</BodyLong>
             <ul>
-                <li>{'Du skal velge "Syk"» hvis du har vært for syk til å kunne delta på tiltaksdagen. Du kan ha rett til tiltakspenger når du er syk. Det er derfor viktig at du melder om dette.'}</li>
+                <li>{'Du skal velge «Syk» hvis du har vært for syk til å kunne delta på tiltaksdagen. Du kan ha rett til tiltakspenger når du er syk. Det er derfor viktig at du melder om dette.'}</li>
                 <li>{'Du får utbetalt full stønad de 3 første dagene du er syk. Er du syk mer enn 3 dager, får du utbetalt 75 prosent av full stønad resten av arbeidsgiverperioden. En arbeidsgiverperiode er på til sammen 16 virkedager.'}</li>
                 <li>{'Du må ha sykmelding fra lege for å ha rett på tiltakspenger i mer enn 3 dager.'}</li>
             </ul>
 
-            <BodyLong spacing>{'Sykt barn eller syk barnepasser'}</BodyLong> 
+            <BodyLong>{'Sykt barn eller syk barnepasser'}</BodyLong> 
             <ul>
                 <li>{'Du skal velge «Sykt barn eller syk barnepasser» hvis du ikke kunne delta på tiltaksdagen fordi barnet ditt eller barnets barnepasser var syk.'}</li>
                 <li>{'Det er de samme reglene som gjelder for sykt barn/barnepasser som ved egen sykdom. Det vil si at du har rett til full utbetaling de tre første dagene og 75 % resten av arbeidsgiverperioden.'}</li>
@@ -44,7 +40,7 @@ export const DeltattHjelp = () => {
                 <li>{'Du skal velge «Annet fravær» hvis du har vært helt eller delvis fraværende den angitte tiltaksdagen.'}</li>
                 <li>{'Du skal velge «Annet fravær» hvis du har fått lønn i den avtalte tiltakstiden.'}</li>
                 <li>{'Du skal velge «Annet fravær» hvis du har fått fri/ferie utenom planlagt ferieperiode for tiltaket.'}</li>
-                <li>{'Du skal også velge «Annet fravær» hvis du har gjennomført aktiviteter som du har avtalt med veilederen din.'}</li>
+                <li><i>{'Du skal også velge «Annet fravær» hvis du har gjennomført aktiviteter som du har avtalt med veilederen din.'}</i>i></li>
             </ul>
 
              <Heading size={'small'} level={'3'}>
@@ -62,7 +58,8 @@ export const DeltattHjelp = () => {
                 <li>{'Du må kunne dokumentere årsaken til fraværet for å få det godkjent. Hvis veilederen din i godkjenner fraværet ditt, vil du få etterbetalt for dagene du har hatt fravær.'}</li>
             </ul>
 
-            
+          <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>
+         <BodyLong>{'Dette er en paragraf uten avstand'}</BodyLong>  
         </ReadMore>
     );
 };
