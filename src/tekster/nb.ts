98c71686bc74cb22113873b5e5e77fc908642ee9
SolviAndersen <37183652+SolviAndersen@users.noreply.github.com>
2025-01-14 13:06:35 +0100

    Update nb.ts

--- src/tekster/nb.ts
+++ src/tekster/nb.ts
@@ -10,7 +10,7 @@ export const teksterNb = {
     statusDeltatt: 'Deltatt',
     statusSyk: 'Syk',
     statusSyktBarn: 'Sykt barn eller syk barnepasser',
-    statusGodkjentFravær: 'Fravær avtalt med Nav',
+    statusGodkjentFravær: 'Fravær godkjent av Nav',
     statusIkkeGodkjentFravær: 'Annet fravær',
 
     sideTittel: 'Meldekort for tiltakspenger',
@@ -48,9 +48,11 @@ export const teksterNb = {
         'Du kan ha rett til tiltakspenger hvis du er for syk til å delta på tiltaksdagen. Ta kontakt med veilederen din for å sjekke hva slags dokumentasjon som kreves.',
     fraværModalSyktBarnIngress:
         'Du kan ha rett på tiltakspenger hvis du ikke kunne delta på tiltaksdagen fordi barnet ditt eller barnets barnepasser var syk. Ta kontakt med veilederen din for å sjekke hva slags dokumentasjon som kreves.',
-    fraværModalAnnetGodkjentIngress: 'Utgår inntil videre',
+    fraværModalAnnetGodkjentIngress: 'Du kan ha rett til tiltakspenger hvis du har gjennomført aktiviteter som er avtalt med og godkjent av veilederen din.',
+      'Godkjente årsaker til fravær, som fortsatt gir deg tiltakspenger, er for eksempel jobbintervju, legetime eller alvorlig sykdom/begravelse i nærmeste familie.',
+        
     fraværModalIkkeGodkjentIngress: [
-        'Du har ikke rett på tiltakspenger hvis du har vært helt eller delvis fraværende fra tiltaket, og fraværet ikke er godkjent av Nav.',
+        'Du har ikke rett på tiltakspenger hvis du har vært fraværende fra tiltaket, og fraværet ikke er godkjent av Nav.',
         'Du kan ha rett til tiltakspenger hvis du har gjennomført aktiviteter som er avtalt med veilederen din. For eksempel jobbintervju eller legetime.',
         'Ta kontakt med veilederen din for å sjekke om du har rett til utbetaling selv om du har hatt fravær.',
     ],
