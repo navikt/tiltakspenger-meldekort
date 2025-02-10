4ea0735f0aa552ff672d7524abb50f0f51446f11
Anders B Nomerstad <anders@nomerstad.com>
2025-02-10 08:58:27 +0100

    Oppdaterer mock-data

--- server/src/fetch/apiFetchMock.ts
+++ server/src/fetch/apiFetchMock.ts
@@ -1,6 +1,7 @@
 import { FetchFraApi } from '@fetch/apiFetch';
 import { MeldekortDagStatus } from '@client/typer/meldekort-utfylling';
 import { MeldekortTilBrukerDTO } from '@client/typer/meldekort-dto';
+import dayjs from 'dayjs';
 
 export const fetchFraApiMock: FetchFraApi = async (_1, path, _2, body) => {
     if (path === 'siste') {
@@ -45,6 +46,10 @@ const mockResponse = (status: number, body: any) =>
         json: () => body,
     }) as unknown as ReturnType<typeof fetchFraApiMock>;
 
+const formatDate = (date: string, plusDays: number = 0) => {
+    return dayjs(date).add(plusDays, 'days').format('YYYY-MM-DD');
+};
+
 const mockMeldekort: MeldekortTilBrukerDTO[] = [
     {
         id: 'meldekort_2',
@@ -53,17 +58,17 @@ const mockMeldekort: MeldekortTilBrukerDTO[] = [
         meldeperiodeKjedeId: 'kjede_2',
         fraOgMed: '2025-01-06',
         tilOgMed: '2025-01-19',
-        maksAntallDager: 6,
+        maksAntallDager: 8,
         dager: [
-            ...Array.from({ length: 4 }).map((_, i) => ({
-                status: MeldekortDagStatus.IkkeRegistrert,
-                harRett: false,
-                dag: `2025-01-0${6 + i}`,
-            })),
             ...Array.from({ length: 10 }).map((_, i) => ({
                 status: MeldekortDagStatus.IkkeRegistrert,
                 harRett: true,
-                dag: `2025-01-1${i}`,
+                dag: formatDate('2025-01-06', i),
+            })),
+            ...Array.from({ length: 4 }).map((_, i) => ({
+                status: MeldekortDagStatus.IkkeRegistrert,
+                harRett: false,
+                dag: formatDate('2025-01-16', i),
             })),
         ],
     },
@@ -74,17 +79,48 @@ const mockMeldekort: MeldekortTilBrukerDTO[] = [
         meldeperiodeKjedeId: 'kjede_1',
         fraOgMed: '2024-12-23',
         tilOgMed: '2025-01-05',
-        maksAntallDager: 6,
+        innsendt: '2025-01-04T07:56:11.995Z',
+        maksAntallDager: 10,
         dager: [
-            ...Array.from({ length: 4 }).map((_, i) => ({
-                status: MeldekortDagStatus.IkkeRegistrert,
-                harRett: false,
-                dag: `2025-01-0${6 + i}`,
+            ...Array.from({ length: 5 }).map((_, i) => ({
+                status: MeldekortDagStatus.Deltatt,
+                harRett: true,
+                dag: formatDate('2024-12-23', i),
             })),
-            ...Array.from({ length: 10 }).map((_, i) => ({
+            ...Array.from({ length: 2 }).map((_, i) => ({
                 status: MeldekortDagStatus.IkkeRegistrert,
                 harRett: true,
-                dag: `2025-01-1${i}`,
+                dag: formatDate('2024-12-28', i),
+            })),
+            {
+                status: MeldekortDagStatus.FraværSyk,
+                harRett: true,
+                dag: formatDate('2024-12-30'),
+            },
+            {
+                status: MeldekortDagStatus.FraværSyktBarn,
+                harRett: true,
+                dag: formatDate('2024-12-31'),
+            },
+            {
+                status: MeldekortDagStatus.Deltatt,
+                harRett: true,
+                dag: formatDate('2025-01-01'),
+            },
+            {
+                status: MeldekortDagStatus.FraværAnnet,
+                harRett: true,
+                dag: formatDate('2025-01-02'),
+            },
+            {
+                status: MeldekortDagStatus.IkkeDeltatt,
+                harRett: true,
+                dag: formatDate('2025-01-03'),
+            },
+            ...Array.from({ length: 2 }).map((_, i) => ({
+                status: MeldekortDagStatus.IkkeRegistrert,
+                harRett: true,
+                dag: formatDate('2025-01-04', i),
             })),
         ],
     },
