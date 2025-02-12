0571a186ab7540a80fa31a60cfe9e7079a188957
Anders B Nomerstad <anders@nomerstad.com>
2025-02-12 12:13:28 +0100

    Fix env file resolver

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env.test') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env.tests') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
--- server/src/validateEnv.ts
+++ server/src/validateEnv.ts
@@ -1,14 +1,4 @@
-const expectedEnv = [
-    'NODE_ENV',
-    'MELDEKORT_API_URL',
-    'MELDEKORT_API_SCOPE',
-    'IDPORTEN_ISSUER',
-    'IDPORTEN_JWKS_URI',
-    'TOKEN_X_ISSUER',
-    'TOKEN_X_TOKEN_ENDPOINT',
-    'TOKEN_X_CLIENT_ID',
-    'TOKEN_X_PRIVATE_JWK',
-] as const;
+const expectedEnv = ['NODE_ENV', 'MELDEKORT_API_URL', 'MELDEKORT_API_SCOPE'] as const;
 
 export const validateEnv = async () => {
     const missingVars: string[] = [];
