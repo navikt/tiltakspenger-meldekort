28883cac494e02045ac0a812f7e7016de8692b72
Henrik Solum <henrik.solum@nav.no>
2025-07-02 16:50:08 +0200

    Revert "1407 Retter opp feil i validering så bruker blir advart om de har svart ja på lønn men ikke huket av for noen dager"
    This reverts commit 737217815961193fa1ef250ead194ac078b2e93d.

--- src/utils/utfyllingValidering.ts
+++ src/utils/utfyllingValidering.ts
@@ -9,15 +9,19 @@ export const antallDagerValidering = (meldekortUtfylling: MeldekortUtfylling) =>
     const antallDagerBesvart = dager?.filter((dag) => dag.status !== IKKE_BESVART).length;
     const harForMangeDagerBesvart = antallDagerBesvart > meldekortUtfylling?.maksAntallDager;
     const harIngenDagerBesvart = antallDagerBesvart === 0;
+    const harIngenDagerMedDeltatt = dager?.filter(
+        (dag) => dag.status !== DELTATT_UTEN_LØNN_I_TILTAKET
+    );
     const harIngenDagerMedFravær =
         dager?.filter((dag) => dagStatusMedFravær.has(dag.status)).length === 0;
     const harIngenDagerMedLønn =
-        dager?.filter((dag) => dag.status === DELTATT_MED_LØNN_I_TILTAKET).length === 0;
+        dager?.filter((dag) => dag.status !== DELTATT_MED_LØNN_I_TILTAKET).length === 0;
 
     return {
         antallDagerBesvart,
         harForMangeDagerBesvart,
         harIngenDagerBesvart,
+        harIngenDagerMedDeltatt,
         harIngenDagerMedFravær,
         harIngenDagerMedLønn,
     };
