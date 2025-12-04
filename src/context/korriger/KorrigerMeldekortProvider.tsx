704e59d08a31a07bc34c7ade9a4837cbd901b741
Henrik Solum <henrik.solum@nav.no>
2025-12-04 09:30:44 +0100

    1803 Fjerner oppdatering av state i en effekt ref "react-hooks/set-state-in-effect"
    https://react.dev/learn/you-might-not-need-an-effect

--- src/context/korriger/KorrigerMeldekortProvider.tsx
+++ src/context/korriger/KorrigerMeldekortProvider.tsx
@@ -1,27 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { KorrigerMeldekortContext } from './KorrigerMeldekortContext';
-import { MeldekortDag, MeldekortDagStatus } from '@common/typer/MeldekortBruker';
+import { MeldekortDagStatus } from '@common/typer/MeldekortBruker';
 import { MeldekortKorrigeringTilUtfylling } from '@common/typer/KorrigerMeldekort.ts';
 
 export const KorrigerMeldekortProvider = (props: { children: React.ReactNode }) => {
     const [utfylling, setUtfylling] = useState<MeldekortKorrigeringTilUtfylling>();
-    const [dager, setDager] = useState<MeldekortDag[]>();
+    const dager = utfylling?.dager ?? [];
 
     const oppdaterDag = (dato: string, status: MeldekortDagStatus) => {
-        setDager((prevDager) => {
-            if (!prevDager) {
-                throw Error('Dager for korrigering er ikke satt');
-            }
+        if (!utfylling) return;
 
-            return prevDager.map((dag) => (dag.dag === dato ? { ...dag, status } : dag));
+        setUtfylling((prev) => {
+            if (!prev) return prev;
+
+            return {
+                ...prev,
+                dager: prev.dager.map((dag) => (dag.dag === dato ? { ...dag, status } : dag)),
+            };
         });
     };
-
-    useEffect(() => {
-        if (utfylling) {
-            setDager(utfylling.dager);
-        }
-    }, [utfylling]);
 
     return (
         <KorrigerMeldekortContext.Provider
