c8d1705de0ab4da0c3db589c23a99f567d250a48
Henrik Solum <henrik.solum@nav.no>
2025-12-04 08:39:56 +0100

    1803 Disable eslint rule for hacky demo side

--- src/components/demo/DemoVarsel.tsx
+++ src/components/demo/DemoVarsel.tsx
@@ -9,6 +9,7 @@ export const DemoVarsel = () => {
     const [isDemoMode, setIsDemoMode] = useState(false);
 
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         setIsDemoMode(window.location.pathname.includes(appConfig.demoRoutePrefix));
     }, []);
 
